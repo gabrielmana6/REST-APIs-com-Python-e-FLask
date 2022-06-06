0a3f52d8fc6da1bff2ec642f75c25f7983bea29c
Gabriel Manassés <gabrielmanasses13@gmail.com>
2022-06-06 16:11:35 -0300

    Fim do Curso

--- models/site.py
+++ models/site.py
@@ -24,10 +24,20 @@ class SiteModel(banco.Model):
             return site
         return None
     
+    @classmethod
+    def find_by_id(cls, site_id):
+        site = cls.query.filter_by(site_id=site_id).first()
+        if site:
+            return site
+        return None
+    
     def save_site(self):
         banco.session.add(self)
         banco.session.commit()
     
     def delete_site(self):
+        # deletando todos hoteis associados ao site
+        [hotel.delete_hotel() for hotel in self.hoteis]
+        # deletando site
         banco.session.delete(self)
         banco.session.commit()
--- resources/hotel.py
+++ resources/hotel.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 from flask_jwt_extended import jwt_required
+from models.site import SiteModel
 import sqlite3
 from resources.filtros import normalize_path_params, consulta_sem_cidade, consulta_com_cidade
 
@@ -67,6 +68,9 @@ class Hotel(Resource):
         
         dados = Hotel.argumentos.parse_args()
         hotel = HotelModel(hotel_id, **dados)
+
+        if not SiteModel.find_by_id(dados['site_id']):
+            return {'message': 'The hotel must be associated to a valid site id.'}, 400
         try:
             hotel.save_hotel()
         except:
