962a48e27566145813389ab370fba7adac5f1ba4
Gabriel Manassés <gabrielmanasses13@gmail.com>
2022-06-23 11:18:05 -0300

    Cadastrando Usuários com Confirmação por Email

--- models/usuario.py
+++ models/usuario.py
@@ -1,25 +1,60 @@
 from sql_alchemy import banco
+from flask import request, url_for
+from requests import post
+
+MAILGUN_DOMAIN = 'sandboxd90e80dbcd3f4429abf9313de0f312d2.mailgun.org'
+MAILGUN_API_KEY = 'e9f59547a5e5e8fb3e1d1baa2ab87549-4f207195-a9517109'
+FROM_TITLE = 'NO-REPLY'
+FROM_EMAIL = 'no-reply@restapi.com'
 
 class UserModel(banco.Model):
     __tablename__ = 'usuarios'
 
     user_id = banco.Column(banco.Integer, primary_key=True)
-    login = banco.Column(banco.String(40))
-    senha = banco.Column(banco.String(40))
+    login = banco.Column(banco.String(40), nullable=False, unique=True)
+    senha = banco.Column(banco.String(40), nullable=False)
+    email = banco.Column(banco.String(80), nullable=False, unique=True)
+    ativado = banco.Column(banco.Boolean, default=False)
 
-    def __init__(self, login, senha):
+    def __init__(self, login, senha, email, ativado):
         self.login = login
         self.senha = senha
+        self.email = email
+        self.ativado = ativado
+    
+    def send_confirmation_email(self):
+        
+        link = request.url_root[:-1] + url_for('userconfirm', user_id=self.user_id)
+        return post('https://api.mailgun.net/v3/{}/messages'.format(MAILGUN_DOMAIN),
+		            auth=('api', MAILGUN_API_KEY),
+		            data={'from':' {} <{}>'.format(FROM_TITLE, FROM_EMAIL),
+			            'to': self.email,
+			            'subject': 'Confirmação de Cadastro',
+			            'text': 'Confirme seu cadastro clicando no link a seguir: {}'.format(link),
+                        'html': '<html><p>\
+                            Confirme seu cadastro clicando no link a seguir: <a href="{}">CONFIRMAR EMAIL</a>\
+                            </p></html>'.format(link)
+                        }
+                    )
     
     def json(self):
         return {
             'user_id': self.user_id,
-            'login':self.login
+            'login': self.login,
+            'email': self.email,
+            'ativado': self.ativado
         }
 
     @classmethod
     def find_user(cls, user_id):
         user = cls.query.filter_by(user_id=user_id).first()
+        if user:
+            return user
+        return None
+    
+    @classmethod
+    def find_by_email(cls, email):
+        user = cls.query.filter_by(email=email).first()
         if user:
             return user
         return None
